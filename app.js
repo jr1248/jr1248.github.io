48fd69fc7bc8366e429d4dd1d72f8e5ff4310c37
jr1248 <55035698+jr1248@users.noreply.github.com>
2020-02-03 17:26:48 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const cast_div= document.getElementById("The Cast");
+const story_div= document.getElementById("Stories");
+const music_div= document.getElementById("Music");
+const about_div= document.getElementById("About");
+const store_div= document.getElementById("Store");
+cast_div.addEventListener('click',function(){
+    console.log("you clicked on cast");
+})
+story_div.addEventListener('click', function(){
+    console.log("you clicked on story");
+})
+music_div.addEventListener('click',function(){
+    console.log("you clicked on music");
+})
+about_div.addEventListener('click',function(){
+    console.log("you clicked on about");
+})
+store_div.addEventListener('click',function(){
+    console.log("you clicked on store");
+})
