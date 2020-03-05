986d120c8f16f7cc286367f9b7487ad6a013d4d3
jr1248 <55035698+jr1248@users.noreply.github.com>
2020-03-05 13:46:22 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-let cast_button= document.getElementById("The-Cast");
-let story_button= document.getElementById("Stories");
-let music_button= document.getElementById("Music");
-let about_button= document.getElementById("About");
-let store_button= document.getElementById("Store");
+const cast_button= document.getElementById("The-Cast");
+
+const story_button= document.getElementById("Stories");
+
+const music_button= document.getElementById("Music");
+
+const about_button= document.getElementById("About");
+
+const store_button= document.getElementById("Store");
+
 function rgb(num) {
     return Math.floor(Math.random() * num);
   }
@@ -12,8 +17,14 @@ function rgb(num) {
     let randomColor = 'rgb(' + rgb(255) + ',' + rgb(255) + ',' + rgb(255) + ')';
     event.target.style.backgroundColor = randomColor;
   }
+
 cast_button.onclick=colorChange;
+
 story_button.onclick=colorChange;
+
 music_button.onclick=colorChange;
+
 about_button.onclick=colorChange;
+
 store_button.onclick=colorChange;
+
